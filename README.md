48f9f52c46664011ef9a9d238128d678b5b2b853
hezouat <zahir.hezouat@fircosoft.com>
2017-12-14 14:56:08 +0100

    Add new project Robot

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# Robot management is a project based on angular 4
+## Robot management is a project based on angular 4
 
+## Launch application
+    1. FrontEnd
+        npm start
+    2. BackEnd
+     task gradle:  bootRun
 
+##Swagger
 You can use directly http://localhost:8080/swagger-ui.html
 
 
--- frontend/src/app/robot/robot.component.spec.ts
+++ frontend/src/app/robot/robot.component.spec.ts
@@ -1,5 +1,9 @@
 import {async, ComponentFixture, TestBed} from "@angular/core/testing";
 import {RobotComponent} from "./robot.component";
+import { Router } from "@angular/router";
+import { Robot } from "../robot";
+import { RobotService } from "../robot.service";
+
 
 describe('RobotComponent', () => {
   let component: RobotComponent;
@@ -23,3 +27,5 @@ describe('RobotComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+
